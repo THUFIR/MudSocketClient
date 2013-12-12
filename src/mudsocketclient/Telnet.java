7569a2fd741664b12710200eafd4c84b890c0587
thufir <thufir@dur.bounceme.net>
2013-12-12 03:47:23 -0800

    executes

--- src/mudsocketclient/Telnet.java
+++ src/mudsocketclient/Telnet.java
@@ -1,6 +1,5 @@
 package mudsocketclient;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,26 +15,40 @@ public class Telnet {
     public static void main(String[] args) throws UnknownHostException, IOException {
         final String host = "rainmaker.wunderground.com";
         final int port = 3000;
-        {
-            Thread remoteConnection = new Thread() {
 
-                @Override
-                public void run() {
-                    int byteOfData;
-                    try (Socket socket = new Socket(host, port);
-                            InputStream inputStream = socket.getInputStream();
-                            OutputStream ouputStream = socket.getOutputStream();
-                            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-                            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-                        while ((byteOfData = inputStream.read()) != -1) {
-                            out.print((char) byteOfData);
-                        }
-                    } catch (Exception e) {
-                        out.println(e);
+
+
+
+        Thread local = new Thread() {
+
+            @Override
+            public void run() {
+            }
+        };
+        
+        
+        
+        
+        
+        Thread remote = new Thread() {
+
+            @Override
+            public void run() {
+                int byteOfData;
+                try (Socket socket = new Socket(host, port);
+                        InputStream inputStream = socket.getInputStream();
+                        OutputStream ouputStream = socket.getOutputStream();
+                        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+                    while ((byteOfData = inputStream.read()) != -1) {
+                        out.print((char) byteOfData);
                     }
+                } catch (Exception e) {
+                    out.println(e);
                 }
-            };
-            remoteConnection.start();
-        }
+            }
+        };
+        remote.start();
+
     }
 }
