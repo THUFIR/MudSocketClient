410a5c81b0275a9286ef1f687b95b95ed6e43900
Thufir <hawat.thufir@gmail.com>
2013-12-15 01:26:19 -0800

    executes

--- src/mudsocketclient/StatelessTriggers.java
+++ src/mudsocketclient/StatelessTriggers.java
@@ -8,9 +8,21 @@ public class StatelessTriggers {
 
     private static Logger log = Logger.getLogger(StatelessTriggers.class.getName());
     private String cmd = null;
+    private String line = null;
 
     public void parse(String line) {
+        this.line = line;
+        if (!line.equalsIgnoreCase(line)) {
+            confuse();
+            fighting();
+        } else {
+            cmd = null;
+            this.line = null;
+            log.fine(line);
+        }
+    }
 
+    private void confuse() {
         if (line.contains("confusing the hell out of")) {
             Pattern pattern = Pattern.compile("[\\w]+(?=\\.)");  //(\w+)\.
             Matcher matcher = pattern.matcher(line);
@@ -28,5 +40,17 @@ public class StatelessTriggers {
 
     public void clear() {
         cmd = null;
+    }
+
+    private void fighting() {
+        if (line.contains("you are fighting")) {
+            Pattern pattern = Pattern.compile("[\\w]+(?=\\.)");  //(\w+)\.
+            Matcher matcher = pattern.matcher(line);
+            String enemy = null;
+            while (matcher.find()) {
+                enemy = matcher.group();
+            }
+            cmd = "confuse" + enemy;
+        }
     }
 }
